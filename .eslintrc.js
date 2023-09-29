4e8d65606e6c280840a775a57762bd8cb438f404
nurzhann173 <nurzhann173@gmail.com>
2023-09-29 19:40:08 +0600

    added lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,35 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true,
-        "es2022": true,
+        "es2021": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
     "overrides": [
         {
             "env": {
-                "node": true,
+                "node": true
             },
             "files": [
                 ".eslintrc.{js,cjs}"
             ],
             "parserOptions": {
                 "sourceType": "script"
-            },
+            }
         }
     ],
+    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
+    "plugins": [
+        "@typescript-eslint"
+    ],
     "rules": {
+        "@typescript-eslint/no-explicit-any": "off",
+        '@typescript-eslint/no-non-null-assertion': 'off'
     }
 }
